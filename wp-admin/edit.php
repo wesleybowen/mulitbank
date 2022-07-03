7ac215f088d4436112f80846c06c733c9540b3e5
wesleybowen <wesbowen@@outlook.com>
2022-07-03 04:58:58 -0500

    Added simply-static-1-1656842081

---
+++ wp-admin/edit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=/hidden-login/?redirect_to=https%3A%2F%2Fwww.dontbankalone.com%2Fwp-admin%2Fedit.php&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "/hidden-login/?redirect_to=https%3A%2F%2Fwww.dontbankalone.com%2Fwp-admin%2Fedit.php&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="/hidden-login/?redirect_to=https%3A%2F%2Fwww.dontbankalone.com%2Fwp-admin%2Fedit.php&reauth=1">/hidden-login/?redirect_to=https%3A%2F%2Fwww.dontbankalone.com%2Fwp-admin%2Fedit.php&reauth=1</a></p>
+	</body>
+</html>
